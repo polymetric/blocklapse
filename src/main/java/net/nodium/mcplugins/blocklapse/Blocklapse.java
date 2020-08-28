592997a3a8f5f9713e7b8b6d2ec316150faabe74
polymetric <polymetricofficial@gmail.com>
2020-08-27 22:33:35 -0400

    initial commit

---
+++ src/main/java/net/nodium/mcplugins/blocklapse/Blocklapse.java
@@ -0,0 +1,13 @@
+package net.nodium.mcplugins.blocklapse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Blocklapse extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
